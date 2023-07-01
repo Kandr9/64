4d385f908fe10079939f227a7d902bca83619fae
Kandr9 <kandr13579@gmail.com>
2023-07-01 03:59:01 +0300

    64

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// 64
+
+
+void NaturalCoint(int a)
+{
+ Console.Write($"{a} ");
+ if (a > 1 )
+ NaturalCoint(a - 1);
+}
+Console.Write("Введите число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+NaturalCoint(b);
